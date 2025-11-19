65e6ac765bcf4fd8edbcdd588be1517e5204a48b
aymane <aymanelhachimi2004@gmail.com>
2025-11-19 19:31:39 +0100

    Changing entities, connecting to sql server and adding migrations

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext( DbContextOptions<HotelListingDbContext> options) : base(options)
+        {
+            
+        }
+
+        DbSet<Country> Countries { get; set; }
+        DbSet<Hotel> Hotels { get; set; }
+    }   
+
+}
